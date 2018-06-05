b882adcb4e58f2658ba6b2cdbc5b78886a44f313
godfery <157062357@qq.com>
2018-06-05 15:33:54 +0800

    commit

--- config/manager.py
+++ config/manager.py
@@ -11,6 +11,6 @@ app.config['SECRET_KEY'] = '12345678'
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@127.0.0.1:3306/article_web"
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['UP'] = os.path.join(os.path.dirname(__file__), 'static/uploads')
+app.config['UP'] = os.path.join(app.root_path, 'static/uploads')
 if __name__ == '__main__':
     print(app.config['UP'])
